5d4589ac2d65bafb7e891da8f6a674194b9c5865
Maxime Puys <drakes00@gmail.com>
2025-09-10 17:38:12 +0200

    Added: JWT auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -22,6 +23,11 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    #[Route('/auth', name: 'app_login_jwt', methods: ['POST'])]
+    public function jwtLogin(): Response
+    {
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
