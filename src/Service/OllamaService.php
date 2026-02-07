c0c1bfd1ebfdba56b37b5adb8024e5b72e141178
Maxime Puys <drakes00@gmail.com>
2026-02-07 21:23:40 +0100

    Added: prompt template

--- src/Service/OllamaService.php
+++ src/Service/OllamaService.php
@@ -6,19 +6,33 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
+use Twig\Environment;
 
 class OllamaService
 {
     private string $ollamaServiceUrl;
+    private int $temperature;
+    private ?int $numCtx;
+    private ?int $topK;
+    private ?float $topP;
     private const CACHE_KEY_MODELS = 'ollama_available_models';
     private const CACHE_TTL_MODELS = 3600; // Cache for 1 hour (3600 seconds)
 
     public function __construct(
         private readonly HttpClientInterface $client,
-        private readonly CacheInterface $cache, // Changed from ItemInterface
-        string $ollamaServiceUrl
+        private readonly CacheInterface $cache,
+        private readonly Environment $twig,
+        string $ollamaServiceUrl,
+        ?int $temperature,
+        ?int $numCtx,
+        ?int $topK,
+        ?float $topP
     ) {
         $this->ollamaServiceUrl = $ollamaServiceUrl;
+        $this->temperature = $temperature;
+        $this->numCtx = $numCtx;
+        $this->topK = $topK;
+        $this->topP = $topP;
     }
 
     /**
@@ -66,14 +80,29 @@ class OllamaService
      */
     public function generateText(string $model, string $prompt): string
     {
+        $finalPrompt = $this->twig->render('prompt.twig', [
+            'prompt' => $prompt,
+        ]);
+
+        $options = [
+            'temperature' => $this->temperature,
+            'num_ctx' => $this->numCtx,
+            'top_k' => $this->topK,
+            'top_p' => $this->topP,
+        ];
+
+        // Filter out null values
+        $options = array_filter($options, fn ($value) => !is_null($value));
+
         $response = $this->client->request(
             'POST',
             $this->ollamaServiceUrl . '/api/generate',
             [
                 'json' => [
                     'model' => $model,
-                    'prompt' => $prompt,
+                    'prompt' => $finalPrompt,
                     'stream' => false, // We want the full response at once
+                    'options' => $options,
                 ],
             ]
         );
@@ -85,8 +114,6 @@ class OllamaService
         }
 
         $data = $response->toArray();
-
-        // The generated text is in the 'response' key
-        return $data['response'] ?? '';
+        return  $data['response'] ?? '';
     }
 }
